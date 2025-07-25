2cc326c0524b289d5e780f217e623a751e5db10b
C. Dylan Shearer <dylan@nekonya.info>
2025-07-25 16:00:18 -0400

    Support single button

--- src/lock/commands.cpp
+++ src/lock/commands.cpp
@@ -2,24 +2,22 @@
 #include <Arduino.h>
 #include <Bounce2.h>
 
-#define PIN_TURN_LEFT 9
-#define PIN_TURN_RIGHT 10
+#define BUTTON_PIN 9
 #define INTERVAL_MS 5
+#define DOUBLE_CLICK_MS 500
 
-static Bounce2::Button gTurnLeftButton, gTurnRightButton;
+static Bounce2::Button gTurnButton;
 static commands::handler_t gHandler = nullptr;
+static unsigned long gLastClickMillis = 0;
 
 void commands::init(handler_t handler) {
     Serial.println("Initializing commands module");
 
     gHandler = handler;
 
-    gTurnLeftButton.attach(PIN_TURN_LEFT, INPUT_PULLUP);
-    gTurnRightButton.attach(PIN_TURN_RIGHT, INPUT_PULLUP);
-    gTurnLeftButton.interval(INTERVAL_MS);
-    gTurnRightButton.interval(INTERVAL_MS);
-    gTurnLeftButton.setPressedState(LOW);
-    gTurnRightButton.setPressedState(LOW);
+    gTurnButton.attach(BUTTON_PIN, INPUT_PULLUP);
+    gTurnButton.interval(INTERVAL_MS);
+    gTurnButton.setPressedState(LOW);
 
     // subscribe to interrupts
     // attachInterrupt(digitalPinToInterrupt(PIN_TURN_LEFT), leftISR, CHANGE);
@@ -27,14 +25,27 @@ void commands::init(handler_t handler) {
 }
 
 void commands::update() {
-    gTurnLeftButton.update();
-    gTurnRightButton.update();
+    gTurnButton.update();
 
-    if (gTurnLeftButton.pressed()) {
-        gHandler(commands::TURN_LEFT);
+    if (gTurnButton.pressed()) {
+        const unsigned long now = millis();
+        if (gLastClickMillis == 0) {
+            // first click
+            gLastClickMillis = now;
+        } else {
+            // second click
+            gLastClickMillis = 0;
+            gHandler(commands::TURN_RIGHT);
+        }
+        return;
     }
 
-    if (gTurnRightButton.pressed()) {
-        gHandler(commands::TURN_RIGHT);
+    if (gLastClickMillis > 0) {
+        // check if the double click timeout has passed
+        if (millis() - gLastClickMillis > DOUBLE_CLICK_MS) {
+            // timeout, reset last click
+            gLastClickMillis = 0;
+            gHandler(commands::TURN_LEFT);
+        }
     }
 }
