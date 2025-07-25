3ce944a924df2df12f6bda39b5bba45c0dad448f
C. Dylan Shearer <dylan@nekonya.info>
2025-07-25 16:06:08 -0400

    Re-activate idle.cpp

--- src/lock/idle.cpp
+++ src/lock/idle.cpp
@@ -2,8 +2,6 @@
 #include <LowPower.h>
 
 void idle() {
-    return;
-
     // Serial.println("Sleeping");
     #if defined (__AVR__)
     LowPower.idle(SLEEP_8S, ADC_ON, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
